c749df02d1d15996599ff9ee80cea4a3ffd7687a
Vitche Research Team <developer@vitche.com>
2017-04-05 19:25:57 +0300

    1. Обновлен источник для получения данных о серверах.

--- configuration.js
+++ configuration.js
@@ -2,7 +2,7 @@ var request = require('request');
 var clusterUri = require('nodejs-cluster-uri');
 module.exports = {
     load: function (callback) {
-        var cluster = new clusterUri.Cluster(['https://dl.dropboxusercontent.com/u/46226853/nodejs-proxy/nodejs-proxy-queue-client.json']);
+        var cluster = new clusterUri.Cluster(['https://raw.githubusercontent.com/vitche/nodejs-storage-configuration/master/nodejs-proxy/nodejs-proxy-queue-client.json']);
         cluster.firstGood(function (error, uri) {
             if (undefined != error) {
                 callback(error);
