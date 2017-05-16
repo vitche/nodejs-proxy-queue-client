76a23336319e37023831e3239da4db37c1a95049
Vitche Research Team <developer@vitche.com>
2017-05-16 20:31:26 +0300

    1. Loading configuration from the root process folder.

--- configuration.js
+++ configuration.js
@@ -1,7 +1,9 @@
 var dotenv = require('dotenv');
 module.exports = {
     load: function (callback) {
-        dotenv.config();
+        dotenv.config({
+            path: process.cwd() + '/.env'
+        });
         var configuration = {
             "torProxyUri": process.env.nodejs_proxy_queue_client__torProxyUri,
             "onionRedisSchema": process.env.nodejs_proxy_queue_client__onionRedisSchema,
